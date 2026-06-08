8db7d1cb263b7c30650dbc0c53b1b368f453abb4
badge-sugawara <badge-sugawara@badge-sugawaranoMacBook-Pro.local>
2026-06-08 15:03:57 +0900

    空文字,スペースが入らないように

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -6,19 +6,41 @@ import { reactive, ref } from 'vue';
 //   'Go to Gym',
 //   'Study JavaScript'
 // ]
+// const todos = reactive([
+//   'Buy Milk',
+//   'Go to Gym',
+//   'Study JavaScript'
+// ]);
 const todos = reactive([
-  'Buy Milk',
-  'Go to Gym',
-  'Study JavaScript'
+  {
+    id: crypto.randomUUID(),
+    title: 'Item 0'
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Item 1'
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Item 2'
+  },
 ]);
 
 // const newTodo = 'new todo';
-const newTodo = ref('new todo');
+const newTodo = ref('');
 
 function addTodo(e) {
+  if (newTodo.value.trim() === '') {
+    return;
+  }
+
   e.preventDefault();
   // alert(newTodo.value);
-  todos.push(newTodo.value)
+  todos.push({
+    id: crypto.randomUUID(),
+    title: newTodo.value
+  })
+  newTodo.value = '';
 }
 
 function handleInput(e) {
@@ -30,7 +52,7 @@ function handleInput(e) {
   <div class="container">
     <h1>Todos</h1>
     <ul>
-      <li v-for="todo in todos">{{ todo }}</li>
+      <li v-for="todo in todos">{{ todo.title }}</li>
     </ul>
     <form @submit="addTodo">
       <!-- <input type="text" :value="newTodo" @input="handleInput"> -->
